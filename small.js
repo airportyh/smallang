93ca5d3eceadd73b933615694f71407c04c9d33a
Toby Ho <airportyh@gmail.com>
2020-07-16 18:16:24 -0400

    Episode 3: parsing function calls and multiple line programs

--- small.js
+++ small.js
@@ -7,7 +7,18 @@ const myLexer = require("./lexer");
 var grammar = {
     Lexer: myLexer,
     ParserRules: [
+    {"name": "statements", "symbols": ["statement"], "postprocess":  
+        (data) => {
+            return [data[0]];
+        }
+                },
+    {"name": "statements", "symbols": ["statements", (myLexer.has("NL") ? {type: "NL"} : NL), "statement"], "postprocess": 
+        (data) => {
+            return [...data[0], data[2]];
+        }
+                },
     {"name": "statement", "symbols": ["var_assign"], "postprocess": id},
+    {"name": "statement", "symbols": ["fun_call"], "postprocess": id},
     {"name": "var_assign", "symbols": [(myLexer.has("identifier") ? {type: "identifier"} : identifier), "_", {"literal":"="}, "_", "expr"], "postprocess": 
         (data) => {
             return {
@@ -17,8 +28,31 @@ var grammar = {
             }
         }
                 },
+    {"name": "fun_call$ebnf$1$subexpression$1", "symbols": ["arg_list", "_"]},
+    {"name": "fun_call$ebnf$1", "symbols": ["fun_call$ebnf$1$subexpression$1"], "postprocess": id},
+    {"name": "fun_call$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "fun_call", "symbols": [(myLexer.has("identifier") ? {type: "identifier"} : identifier), "_", {"literal":"("}, "_", "fun_call$ebnf$1", {"literal":")"}], "postprocess": 
+        (data) => {
+            return {
+                type: "fun_call",
+                fun_name: data[0],
+                arguments: data[4] ? data[4][0] : []
+            }
+        }
+                },
+    {"name": "arg_list", "symbols": ["expr"], "postprocess": 
+        (data) => {
+            return [data[0]];
+        }
+                },
+    {"name": "arg_list", "symbols": ["arg_list", "__", "expr"], "postprocess": 
+        (data) => {
+            return [...data[0], data[2]];
+        }
+                },
     {"name": "expr", "symbols": [(myLexer.has("string") ? {type: "string"} : string)], "postprocess": id},
     {"name": "expr", "symbols": [(myLexer.has("number") ? {type: "number"} : number)], "postprocess": id},
+    {"name": "expr", "symbols": [(myLexer.has("identifier") ? {type: "identifier"} : identifier)], "postprocess": id},
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", (myLexer.has("WS") ? {type: "WS"} : WS)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "_", "symbols": ["_$ebnf$1"]},
@@ -26,7 +60,7 @@ var grammar = {
     {"name": "__$ebnf$1", "symbols": ["__$ebnf$1", (myLexer.has("WS") ? {type: "WS"} : WS)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "__", "symbols": ["__$ebnf$1"]}
 ]
-  , ParserStart: "statement"
+  , ParserStart: "statements"
 }
 if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
    module.exports = grammar;
--- small.ne
+++ small.ne
@@ -4,9 +4,24 @@ const myLexer = require("./lexer");
 
 @lexer myLexer
 
+statements
+    -> statement
+        {% 
+            (data) => {
+                return [data[0]];
+            }
+        %}
+    |  statements %NL statement
+        {%
+            (data) => {
+                return [...data[0], data[2]];
+            }
+        %}
+
 statement
     -> var_assign  {% id %}
-    
+    |  fun_call    {% id %}
+
 var_assign
     -> %identifier _ "=" _ expr
         {%
@@ -18,10 +33,37 @@ var_assign
                 }
             }
         %}
-    
+
+fun_call
+    -> %identifier _ "(" _ (arg_list _):? ")"
+        {%
+            (data) => {
+                return {
+                    type: "fun_call",
+                    fun_name: data[0],
+                    arguments: data[4] ? data[4][0] : []
+                }
+            }
+        %}
+
+arg_list
+    -> expr
+        {%
+            (data) => {
+                return [data[0]];
+            }
+        %}
+    |  arg_list __ expr
+        {%
+            (data) => {
+                return [...data[0], data[2]];
+            }
+        %}
+
 expr
     -> %string     {% id %}
     |  %number     {% id %}
+    |  %identifier {% id %}
 
 # Optional whitespace    
 _ -> %WS:*
