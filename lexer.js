fa6f68dcfea2537a04fdcfdb339e81f0fde7b298
Toby Ho <airportyh@gmail.com>
2020-07-16 17:04:20 -0400

    Episode 1: The Lexer

---
+++ lexer.js
@@ -0,0 +1,32 @@
+const moo = require('moo');
+const fs = require("mz/fs");
+
+let lexer = moo.compile({
+  WS:      /[ \t]+/,
+  comment: /\/\/.*?$/,
+  number:  /0|[1-9][0-9]*/,
+  string:  /"(?:\\["\\]|[^\n"\\])*"/,
+  lparen:  '(',
+  rparen:  ')',
+  lbrace:  '{',
+  rbrace:  '}',
+  identifier: /[a-zA-Z][a-zA-Z_0-9]*/,
+  fatarrow: '=>',
+  assign: '=',
+  NL:      { match: /\n/, lineBreaks: true },
+});
+
+async function main() {
+    const code = (await fs.readFile("example.small")).toString();
+    lexer.reset(code);
+    while (true) {
+        const token = lexer.next();
+        if (!token) {
+            break;
+        }
+        console.log(token);
+    }
+
+}
+
+main().catch(err => console.log(err.stack));
