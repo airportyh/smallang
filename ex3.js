d5b7c24ff88872b106abcae84f1a9fc65c14d6a1
Toby Ho <airportyh@gmail.com>
2020-07-17 12:17:43 -0400

    Episode 5: Extra runtime functions

--- ex3.js
+++ ex3.js
@@ -1,3 +1,9 @@
+var x1 = 4;
+var y1 = 8;
+var x2 = 5;
+var y2 = 12;
+var dist = sqrt(add(pow(subtract(x1, x2), 2), pow(subtract(y1, y2), 2)));
+print("The distance is", dist)
 function print(...args) {
     console.log(...args);
 }
